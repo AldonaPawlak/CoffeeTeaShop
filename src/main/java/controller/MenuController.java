0444716a8c8637ce3bb6cd01d441043904e8a302
Marta Skarbek <marta@skarbek.name>
2020-05-19 02:12:31 +0200

    added constructor

--- src/main/java/controller/MenuController.java
+++ src/main/java/controller/MenuController.java
@@ -1,4 +1,17 @@
 package controller;
 
+import com.sun.tools.javac.comp.Todo;
+import controller.DAO.UserDAO;
+import model.User;
+import view.UI;
+
 public class MenuController {
+
+    private UserDAO userDao;
+    private User user;
+    private UI ui;
+
+    public void createUserProfileMenu() {
+        // TODO
+    }
 }
