dcddfb2ae46366ae97d8fa8e3520b3782d6b0d4b
Marta Skarbek <marta@skarbek.name>
2020-05-19 02:16:27 +0200

    added getters

--- src/main/java/controller/MenuController.java
+++ src/main/java/controller/MenuController.java
@@ -11,7 +11,25 @@ public class MenuController {
     private User user;
     private UI ui;
 
+    public MenuController(UserDAO userDao, User user, UI ui) {
+        this.userDao = userDao;
+        this.user = user;
+        this.ui = ui;
+    }
+
     public void createUserProfileMenu() {
         // TODO
+    }
+
+    public UserDAO getUserDao() {
+        return userDao;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public UI getUi() {
+        return ui;
     }
 }
