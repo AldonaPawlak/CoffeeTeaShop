b5092d8d05e311f4916add392c304523806b56b7
Mat191126 <matprz11@gmail.com>
2020-05-19 16:10:37 +0200

    Implemented select and insertInto methods in UserDAO

--- src/main/java/controller/DAO/UserDAO.java
+++ src/main/java/controller/DAO/UserDAO.java
@@ -1,9 +1,42 @@
 package controller.DAO;
 
+import java.sql.*;
+
 public class UserDAO extends DataBaseDAO implements DAO{
+
+    public UserDAO(){
+    }
     @Override
     public void select() {
-        connect();
+        try{
+            connect();
+            connection.setAutoCommit(false);
+            ResultSet rs = statement.executeQuery( "SELECT * FROM Users;" );
+            while ( rs.next() ) {
+                int id = rs.getInt("Id_user");
+                String  name = rs.getString("Name");
+                String surname  = rs.getString("Surname");
+                String mail = rs.getString("e-mail");
+                String password = rs.getString("Password");
+                int phone = rs.getInt("Phone");
+                String rights = rs.getString("rights");
+
+                System.out.println( "ID = " + id );
+                System.out.println( "NAME = " + name );
+                System.out.println( "SURNAME = " + surname );
+                System.out.println( "E-MAIL = " + mail );
+                System.out.println( "Password = " + password );
+                System.out.println("Phone = " + phone);
+                System.out.println("rights = " + rights);
+                System.out.println();
+            }
+            rs.close();
+            statement.close();
+            connection.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -18,6 +51,18 @@ public class UserDAO extends DataBaseDAO implements DAO{
 
     @Override
     public void insertInto() {
+        try {
+            connect();
+            String sql = "INSERT INTO Users (Id_user,Name,Surname,e-mail,Password,Phone,rights) " +
+                    "VALUES (2, 'Paul', 'Nowak', 'nowak@onet.pl', 'nowak1234', 123456789,'customer');";
+            statement.executeUpdate(sql);
 
+            statement.close();
+            connection.commit();
+            connection.close();
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
     }
 }
