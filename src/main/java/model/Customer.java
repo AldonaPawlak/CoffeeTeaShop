068d28c006c7a5550c3ef7f793b56ba3b358113b
wtokarz80 <w.tokarz80gmail.com>
2020-05-19 11:09:51 +0200

    add product and user

--- src/main/java/model/Customer.java
+++ src/main/java/model/Customer.java
@@ -1,4 +1,20 @@
 package model;
 
 public class Customer {
+    private String phone;
+    private String address;
+
+    public Customer(String phone, String address){
+        this.phone = phone;
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
 }
+
