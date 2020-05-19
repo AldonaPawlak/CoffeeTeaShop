a9b874cd5c6f79b1fa5d285ee8886ab0dc9976c6
Marta Skarbek <marta@skarbek.name>
2020-05-19 10:54:02 +0200

    added constructor

--- src/main/java/model/Cart.java
+++ src/main/java/model/Cart.java
@@ -1,4 +1,53 @@
 package model;
 
+import java.util.Map;
+
 public class Cart {
+    private Integer id;
+    private Map<Product, Integer> products;
+    Product product;
+
+    public Cart(int id, Map<Product, Integer> products) {
+        this.id = id;
+        this.products = products;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Map<Product, Integer> products() {
+        return products;
+    }
+
+    public void addToCart() {
+        // TODO
+    }
+
+    public void editCart() {
+        // TODO
+    }
+
+    public void emptyCart() {
+        // TODO
+    }
+
+    public void removeFromCart() {
+        // TODO
+    }
+
+    public void getProducts() {
+        // TODO
+    }
+
+    public void countTotalPrice() {
+        // TODO
+    }
+
+
+
+
+
+
+
 }
