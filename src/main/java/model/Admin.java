9f647850cd0ec25516c52a6f5f32e1d328095025
Marta Skarbek <marta@skarbek.name>
2020-05-19 10:58:33 +0200

    added getters

--- src/main/java/model/Admin.java
+++ src/main/java/model/Admin.java
@@ -1,5 +1,41 @@
 package model;
 
-public class Admin {
+public class Admin extends User {
+    private int id;
+    private String email;
+    private String password;
+    private String name;
+    private String surname;
+
+    public Admin(int id, String email, String password, String name, String surname) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+
+
 
 }
