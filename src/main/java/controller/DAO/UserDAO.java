84934e055159f05a8866ef331a3641efaacb42a5
Mat191126 <matprz11@gmail.com>
2020-05-19 15:10:16 +0200

    DAO refactor

--- src/main/java/controller/DAO/UserDAO.java
+++ src/main/java/controller/DAO/UserDAO.java
@@ -1,4 +1,23 @@
 package controller.DAO;
 
-public class UserDAO {
+public class UserDAO extends DataBaseDAO implements DAO{
+    @Override
+    public void select() {
+        connect();
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void delete() {
+
+    }
+
+    @Override
+    public void insertInto() {
+
+    }
 }
