33f87b3fbfa48a8ea49c651015867b45b1c01779
Your Name <you@example.com>
2020-05-20 21:43:36 +0200

    create add new product category method

--- src/main/java/controller/AdminMenuController.java
+++ src/main/java/controller/AdminMenuController.java
@@ -1,4 +1,15 @@
 package controller;
+import view.Input;
+import controller.DAO.ProductCategoryDAO;
 
 public class AdminMenuController {
+
+    Input input = new Input();
+    ProductCategoryDAO productCategoryDAO = new ProductCategoryDAO();
+
+    private void addProductCategory() {
+        System.out.println("ADDING NEW PRODUCT CATEGORY");
+        String name = input.getStringInput("Enter a name for the category: ");
+        productCategoryDAO.insertInto();
+    }
 }
