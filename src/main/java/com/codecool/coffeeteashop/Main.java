125b096ff5e0f4113957fe605f3901affc4c566b
Mat191126 <matprz11@gmail.com>
2020-05-21 11:57:22 +0200

    Throw declaration in Main added

--- src/main/java/com/codecool/coffeeteashop/Main.java
+++ src/main/java/com/codecool/coffeeteashop/Main.java
@@ -1,7 +1,9 @@
 package com.codecool.coffeeteashop;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Online (not online) 'CoffeTea' shop");
         Program program = new Program();
         program.runProgram();
