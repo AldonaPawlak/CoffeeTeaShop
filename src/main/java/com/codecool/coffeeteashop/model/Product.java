32dd27090c1e974cdc392ca5e8e4db78d63a23f2
Mat191126 <matprz11@gmail.com>
2020-05-21 14:39:55 +0200

    Added rating field, getter and setter in Product class, added Rates and Number_of_rates columns in ProductDatabase

--- src/main/java/com/codecool/coffeeteashop/model/Product.java
+++ src/main/java/com/codecool/coffeeteashop/model/Product.java
@@ -8,6 +8,7 @@ public class Product {
     private int quantity;
     private final int categoryId;
     private final Boolean isAvailable;
+    private int rate = 0;
 
 
     public Product(int id, String name, float price, int quantity, int categoryId, Boolean isAvailable) {
@@ -45,5 +46,13 @@ public class Product {
 
     public Boolean getAvailable() {
         return isAvailable;
+    }
+
+    public int getRate() {
+        return rate;
+    }
+
+    public void setRate(int rate) {
+        this.rate = rate;
     }
 }
