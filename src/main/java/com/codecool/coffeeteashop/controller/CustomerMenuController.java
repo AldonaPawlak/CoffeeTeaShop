81d30460af0d555e2849116f1cdd442ff963cf36
Marta Skarbek <marta@skarbek.name>
2020-05-21 02:04:21 +0200

    added part of main logic

--- src/main/java/com/codecool/coffeeteashop/controller/CustomerMenuController.java
+++ src/main/java/com/codecool/coffeeteashop/controller/CustomerMenuController.java
@@ -1,4 +1,17 @@
 package com.codecool.coffeeteashop.controller;
 
+import com.codecool.coffeeteashop.view.Input;
+import com.codecool.coffeeteashop.view.UI;
+
 public class CustomerMenuController {
+    UI ui = new UI();
+    Input input = new Input();
+
+    public CustomerMenuController() {
+    }
+
+    public void userOptions() {
+        ui.printUserMenu();
+        final int userChoice = input.getIntegerInput("What do you want to do right now?");
+    }
 }
