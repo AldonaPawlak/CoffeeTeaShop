4e673c580bd038f224a2a5210053df0b452588b8
Marta Skarbek <marta@skarbek.name>
2020-05-21 07:19:37 +0200

    refactor

--- src/main/java/com/codecool/coffeeteashop/Program.java
+++ src/main/java/com/codecool/coffeeteashop/Program.java
@@ -5,12 +5,9 @@ import com.codecool.coffeeteashop.controller.DAO.ProductCategoryDAO;
 import com.codecool.coffeeteashop.controller.DAO.ProductDAO;
 import com.codecool.coffeeteashop.controller.DAO.UserDAO;
 import com.codecool.coffeeteashop.controller.Login;
-import com.codecool.coffeeteashop.controller.MenuController;
 import com.codecool.coffeeteashop.controller.Registration;
 import com.codecool.coffeeteashop.view.Input;
 import com.codecool.coffeeteashop.view.UI;
-
-import java.util.Arrays;
 
 public class Program {
     private Input input = new Input();
